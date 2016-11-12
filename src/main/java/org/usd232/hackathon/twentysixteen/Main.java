db7b413a4734964bb14b167c4b0c97ba615721b6
codymoose <rcmoose2000@gmail.com>
2016-11-12 02:23:16 -0600

    Added get /authStatus

--- src/main/java/org/usd232/hackathon/twentysixteen/Main.java
+++ src/main/java/org/usd232/hackathon/twentysixteen/Main.java
@@ -1,9 +1,23 @@
 package org.usd232.hackathon.twentysixteen;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import org.usd232.hackathon.twentysixteen.authentication.IAuthenticationService;
+import org.usd232.hackathon.twentysixteen.authentication.UserInfo;
 
-public class Main {
-	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello World");
-	}
+public class Main
+{
+    // TODO Set auth to something
+    static IAuthenticationService auth;
+
+    public static void main(String[] args)
+    {
+        get("/hello", (req, res)->"Hello World");
+        get("/authStatus", (req, res)->
+        {
+            boolean authed = auth.isAuthenticated(req.attribute("X-Auth-Token").toString());
+            UserInfo user = auth.getInfo(req.attribute("X-Auth-Token").toString());
+            String json = "{\"authed\":" + authed + ",\"fname\":\"" + user.getFirstname() + "\",\"lname\":\"" + user.getLastname() + "\"}";
+            return json;
+        });
+    }
 }

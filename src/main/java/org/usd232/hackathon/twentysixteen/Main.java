2ca7017ca2e3810359848e7637b5e333b344ebe5
codymoose <rcmoose2000@gmail.com>
2016-11-12 03:33:14 -0600

    Added /accounts and /accounts/GUID to Main API. Various "req.*" will need to be changed when other parts are added.

--- src/main/java/org/usd232/hackathon/twentysixteen/Main.java
+++ src/main/java/org/usd232/hackathon/twentysixteen/Main.java
@@ -19,5 +19,28 @@ public class Main
             String json = "{\"authed\":" + authed + ",\"fname\":\"" + user.getFirstname() + "\",\"lname\":\"" + user.getLastname() + "\"}";
             return json;
         });
+        get("/accounts", (req, res)->{
+            //TODO Fix req.* stuff
+           String json = "[";
+           int accountsNum = req.NUMBER_OF_ACCOUNTS;
+           for(int i = 0; i < accountsNum; i++){
+               String thisAccountJSON = "{\"type\":\"" + req.ACCOUNTS[i].TYPE + "\",\"name\":\"" + req.ACCOUNTS[i].NAME + "\",\"category\":\"" + req.ACCOUNTS[i].CATEGORY + "\",\"id\":\"" + req.ACCOUNTS[i].GUID + "\"},";
+               json += thisAccountJSON;
+           }
+           json = json.substring(0, json.length() - 2);
+           json += "]";
+           return json;
+        });
+        get("/accounts/GUID", (req, res)->{
+            //TODO Fix req.* stuff
+           String json = "{\"type\":\"" + req.ACCOUNT.GUID.TYPE + "\",\"name\":\"" + req.ACCOUNT.GUID.NAME + "\",\"category\":\"" + req.ACCOUNT.GUID.CATEGORY + "\"";
+           json += ",\"chats\":[";
+           for(int i = 0; i < req.ACCOUNT.GUID.CHATS.length; i++){
+               json += "{\"type:\"" + req.ACCOUNT.GUIDE.CHATS[i].TYPE + "\"name:\"" + req.ACCOUNT.GUIDE.CHATS[i].NAME + "\"id:\"" + req.ACCOUNT.GUIDE.CHATS[i].GUID + "\"},";
+           }
+           json = json.substring(0, json.length() - 2);
+           json += "]}";
+           return json;
+        });
     }
 }
